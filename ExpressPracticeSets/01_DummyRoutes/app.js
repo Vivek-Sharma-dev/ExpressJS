f54617a21d8a8f7e8eeb752dc5472cd467d979b9
Vivek Sharma <vivek.fsd@gmail.com>
2026-07-19 22:48:05 +0530

    basic setup of dummy route task

---
+++ ExpressPracticeSets/01_DummyRoutes/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+
+const app = express();
+const PORT = 3000;
+
+app.use('/', )
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`server is running on http://localhost:${PORT}`);
+})
